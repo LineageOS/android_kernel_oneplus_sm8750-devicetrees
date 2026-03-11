75a412430899e0ec79751fb0b86e61d046a6032b
kmiit <kmiit@foxmail.com>
2026-03-11 04:30:16 +0000

    treewide: Import pista changes from RMX5010_16.0.0.211(CN01)
    Change-Id: I1c72b2050168c1510473533e15134947bd56c58a
    Signed-off-by: kmiit <kmiit@foxmail.com>

--- qcom/camera/config/sun.mk
+++ qcom/camera/config/sun.mk
@@ -25,4 +25,7 @@ dtbo-$(CONFIG_ARCH_SUN)   += oplus/paganiIn-camera-overlay-T0.dtbo \
 dtbo-$(CONFIG_ARCH_SUN)   += oplus/piloti-camera-overlay-T0.dtbo \
 			     oplus/piloti-camera-overlay-evb.dtbo \
 
+dtbo-$(CONFIG_ARCH_SUN)   += oplus/pista-camera-overlay-evb.dtbo \
+			     oplus/pista-camera-overlay-T0.dtbo \
+
 dtbo-$(CONFIG_ARCH_SUN)   += oplus/ktm-camera-overlay-T0.dtbo \
